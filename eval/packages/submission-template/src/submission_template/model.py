a5e98e0fa98770e9d23c3ae27da715c19597bed0
Dick Ameln <dick.ameln@intel.com>
2025-04-04 14:42:19 +0200

    implement winclip-based sample submission

--- eval/packages/submission-template/src/submission_template/model.py
+++ eval/packages/submission-template/src/submission_template/model.py
@@ -2,11 +2,16 @@
 
 import torch
 from anomalib.data import ImageBatch
+from anomalib.models.image.winclip.torch_model import WinClipModel
 from torch import nn
 
 
 class Model(nn.Module):
     """TODO: Implement your model here"""
+    def __init__(self):
+        super().__init__()
+        self.winclip = WinClipModel()
+        self.transform = self.winclip.transform
 
     def setup(self, setup_data: dict[str, torch.Tensor]) -> None:
         """Setup the model.
@@ -16,7 +21,16 @@ class Model(nn.Module):
         Args:
             setup_data (dict[str, torch.Tensor]): The setup data.
         """
-        pass
+        # get class name
+        class_name = setup_data.get("dataset_category")
+        # get few shot images
+        images = setup_data.get("few_shot_samples")
+        if images is not None:
+            device = images.device
+            images = torch.stack([self.winclip.transform(image) for image in images])
+            images = images.to(device)
+        # setup model
+        self.winclip.setup(class_name, images)
 
     def weights_url(self, category: str) -> str | None:
         """URL to the model weights.
@@ -36,8 +50,11 @@ class Model(nn.Module):
             ImageBatch: The output image batch.
         """
         # TODO: Implement the forward pass of the model.
+        device = image.device
+        image = self.winclip.transform(image.squeeze())
+        predictions = self.winclip(image.unsqueeze(0).to(device))
         return ImageBatch(
             image=image,
-            pred_score=torch.zeros(image.shape[0]),
-            anomaly_map=torch.zeros(1, *image.shape[2:]),
+            pred_score=predictions.pred_score,
+            anomaly_map=predictions.anomaly_map,
         )
