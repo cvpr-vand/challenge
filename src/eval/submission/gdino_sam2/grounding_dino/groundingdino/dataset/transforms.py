cab752ee9c185cd282d2622970a4e8ffe5f4fb3e
10072023 <wangtingt@boe.com.cn>
2025-05-15 17:36:18 +0800

    5-6

--- src/eval/submission/gdino_sam2/grounding_dino/groundingdino/dataset/transforms.py
+++ src/eval/submission/gdino_sam2/grounding_dino/groundingdino/dataset/transforms.py
@@ -10,8 +10,8 @@ import torch
 import torchvision.transforms as T
 import torchvision.transforms.functional as F
 
-from submission.gdino_sam2.grounding_dino.groundingdino.util.box_ops import box_xyxy_to_cxcywh
-from submission.gdino_sam2.grounding_dino.groundingdino.util.misc import interpolate
+from groundingdino.util.box_ops import box_xyxy_to_cxcywh
+from groundingdino.util.misc import interpolate
 
 
 def crop(image, target, region):
