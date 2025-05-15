07d32468f48772270114ce15468755ed9b774942
10072023 <wangtingt@boe.com.cn>
2025-05-15 16:57:34 +0800

    4-7

--- src/eval/submission/gdino_sam2/grounding_dino/groundingdino/util/inference.py
+++ src/eval/submission/gdino_sam2/grounding_dino/groundingdino/util/inference.py
@@ -8,9 +8,9 @@ from PIL import Image
 from torchvision.ops import box_convert
 import bisect
 import groundingdino
+from groundingdino.models import build_model
 from groundingdino.datasets import transforms as T
 #import groundingdino.datasets.transforms as T
-from groundingdino.models import build_model
 from groundingdino.util.misc import clean_state_dict
 from groundingdino.util.slconfig import SLConfig
 from groundingdino.util.utils import get_phrases_from_posmap
