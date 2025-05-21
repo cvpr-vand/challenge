5c2eaf78b84449332a1edcbc8fd60b286423c076
BoeAIoT <18810326697@163.com>
2025-05-21 10:11:36 +0800

    boe-aiot4

--- src/eval/submission/gdino_sam2/grounding_dino/groundingdino/util/get_tokenlizer.py
+++ src/eval/submission/gdino_sam2/grounding_dino/groundingdino/util/get_tokenlizer.py
@@ -17,7 +17,8 @@ def get_tokenlizer(text_encoder_type):
     print("final text_encoder_type: {}".format(text_encoder_type))
     current_dir = os.path.dirname(os.path.abspath(__file__))
 
-    tokenizer = AutoTokenizer.from_pretrained(os.path.join(current_dir, "../../../checkpoints/google-bert/bert-base-uncased"))
+    #tokenizer = AutoTokenizer.from_pretrained(os.path.join(current_dir, "../../../checkpoints/google-bert/bert-base-uncased"))
+    tokenizer = AutoTokenizer.from_pretrained(text_encoder_type)
     # print(tokenizer)
     return tokenizer
 
@@ -25,7 +26,8 @@ def get_tokenlizer(text_encoder_type):
 def get_pretrained_language_model(text_encoder_type):
     if text_encoder_type == "bert-base-uncased" or (os.path.isdir(text_encoder_type) and os.path.exists(text_encoder_type)):
         current_dir = os.path.dirname(os.path.abspath(__file__))
-        return BertModel.from_pretrained(os.path.join(current_dir, "../../../checkpoints/google-bert/bert-base-uncased"))
+        #return BertModel.from_pretrained(os.path.join(current_dir, "../../../checkpoints/google-bert/bert-base-uncased"))
+        return BertModel.from_pretrained(text_encoder_type)
     if text_encoder_type == "roberta-base":
         return RobertaModel.from_pretrained(text_encoder_type)
 
